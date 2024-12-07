7d69643896b66cb15d9af7e1ba54501f7ddf1151
PasanSasmika <pasansasmika198@gmail.com>
2024-12-07 08:53:07 +0530

    create product model, product controller and create user validation function.📑🎮

---
+++ controllers/productController.js
@@ -0,0 +1,33 @@
+import Product from "../models/products.js";
+import { isAdmin } from "./userController.js";
+
+export  function createProduct(req,res){
+
+    if(!isAdmin(req)){
+        res.json({
+            message: "Please login as an admin to add product.!" 
+        })
+        return
+    }
+
+    const newProductData = req.body
+
+    const product = new Product(newProductData)
+
+    product.save().then(()=>{
+        res.json({
+            message: "Product added..!"
+        })
+    }).catch((error)=>{
+        res.json({
+            message: error
+        })
+    })
+
+}
+
+export function getProduct(req,res){
+    Product.find({}).then((products)=>{
+        res.json(products)
+    })
+}
