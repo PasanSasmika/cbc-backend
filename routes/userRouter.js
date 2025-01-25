ad9a39f8d07aefd76130efd2aa0cdae991c05538
PasanSasmika <pasansasmika198@gmail.com>
2025-01-25 06:54:11 +0530

    create google login✨

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import { creatUser, loginUser } from '../controllers/userController.js';
+import { creatUser, googleLogin, loginUser } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.post("/",creatUser)
 
 userRouter.post("/login",loginUser)
+
+userRouter.post("/google",googleLogin)
+
 
 export default userRouter;
